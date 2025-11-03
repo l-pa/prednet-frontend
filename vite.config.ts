6ff3d65de832b9a9e48ab6ce458be80911cbff85
Lukas Papik <papikdev@gmail.com>
2025-11-03 17:37:52 +0100

    ignore warnings

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import { tanstackRouter } from "@tanstack/router-plugin/vite"
 import react from "@vitejs/plugin-react-swc"
 import { defineConfig } from "vite"
 
+if (process.env.SKIP_TS_CHECK === "true") {
+  process.env.TSC_COMPILE_ON_ERROR = "true"
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
